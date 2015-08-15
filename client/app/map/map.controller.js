aadc011f60f993ada324f45bc59c89ea7b4131b7
Corey White <smortopahri@gmail.com>
2015-08-15 17:58:32 -0400

    Added click event to map

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('plottGisApp')
   .controller('MapCtrl', function ($scope, $http, socket) {
-    
+
     mapboxgl.accessToken = 'pk.eyJ1IjoiY3R3aGl0ZSIsImEiOiItb0dqdUlZIn0.4Zb1DGESXnx0ePxMVLihZQ';
     if (!mapboxgl.supported()) {
       alert('Your browser does not support Mapbox GL');
@@ -14,14 +14,24 @@ angular.module('plottGisApp')
         zoom: 17 // starting zoom
       });
 
-      // Add zoom and rotation controls to the map.
-      map.addControl(new mapboxgl.Navigation());
+      //On click get interment data
+      map.on('click', function(e) {
+       map.featuresAt(e.point, {radius: 10, layer: 'interments', type: 'vector'}, function(err, features) {
+           if (err) throw err;
+           console.log(features);
+          //  document.getElementById('features').innerHTML = JSON.stringify(features, null, 2);
+           $scope.clickFeature = features[0].properties;
+           console.log($scope.clickFeature);
+       });
+      });
+    }
 
       $http.get('/api/interments').success(function(graves) {
         $scope.graves = graves;
+        console.log('GET Interment', graves);
         socket.syncUpdates('interments', $scope.graves);
         map.on('style.load', function() {
-          map.addSource("markers", {
+          map.addSource("interments", {
             "type": "geojson",
             "data": {
               "type": "FeatureCollection",
@@ -29,23 +39,27 @@ angular.module('plottGisApp')
             }
           });
           map.addLayer({
-           "id": "markers",
+           "id": "interments",
            "type": "symbol",
-           "source": "markers",
+           "source": "interments",
+           "interactive": true,
            "layout": {
-             "icon-image": "{marker-symbol}-12",
+             "icon-image": "monument-12",
              "text-field": "{last_name}",
              "text-font": "Open Sans Semibold, Arial Unicode MS Bold",
              "text-offset": [0, 0.6],
              "text-anchor": "top"
            },
            "paint": {
-             "text-size": 12
+             "text-size": 12,
+             "text-color": "#fff",
+             "icon-color": "#fff"
            }
          });
        });
      });
-   }
+
+
 
     $scope.addThing = function() {
       if($scope.newGrave === '') {
